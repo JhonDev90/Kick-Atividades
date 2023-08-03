e4c22c320bb3ca4422664abb3a701add186a0d16
João Vitor Ferreira de Sousa <118639436+JhonDev90@users.noreply.github.com>
2023-08-03 18:49:22 -0300

    Atividade da parada 42

---
+++ Lista de Exercícios/exercício 4.py
@@ -0,0 +1,7 @@
+#Faça um prorama que converte centímetros em metros.
+
+metros = float(input("Digite os metros?"))
+
+centimetros = metros * 100
+
+print(centimetros)
---
+++ Lista de Exercícios/exercício 5.py
@@ -0,0 +1,8 @@
+#Faça um programa que calcula a área de um quadrado/etangulo
+
+base = float(input("Digite a medida da base: "))
+altura = float(input("Digite a medida da altura: "))
+
+area = base * altura
+
+print("A área do quadrado/retângulo é:", area)
